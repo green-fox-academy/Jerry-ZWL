8d937f9d973c31429672abce40e62d586ee58cd3
Jerry-ZWL <yenmoon@icloud.com>
2017-08-28 08:54:39 +0800

    move things here and revised exercises in html-css-bascis-and-forms

--- week02/day-01/manipulations.sh
+++
@@ -1,9 +0,0 @@
-cd git-lessons-repository
-ls -lah
-cp index.html about.html
-mkdir temp_images
-cp ./images/1.jpg ./images/2.jpg ./temp_images/
-cp ./images/. ./temp_images/
-mv ./css/6.jpg ./images/
-rm -rf ./css/7.jpg
-
